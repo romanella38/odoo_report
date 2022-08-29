0a09991d63cc06bc93c8756911ba1c3d78e0103f
Carlos <carlosjmeilan@gmail.com>
2022-08-29 12:45:05 -0300

    Update __manifest__.py

--- journales/__manifest__.py
+++ journales/__manifest__.py
@@ -27,7 +27,6 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/asset_prueba.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
